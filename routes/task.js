5c953ed6ed37bfd81ac9b50ecfec7c674d0ee6ab
yash yadav <132824732+yaduwanshiyash@users.noreply.github.com>
2024-03-22 09:49:17 +0530

    updated

---
+++ routes/task.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose')
+
+
+const taskSchema = mongoose.Schema({
+    title: {
+        type:String,
+    },
+    task: {
+        type: String
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+
+module.exports = mongoose.model("task",taskSchema)
